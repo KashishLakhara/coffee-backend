5160ea04e786fc01b6c81c47b463ffa8076b7b9a
Kashish Lakhara <kashiish19@gmail.com>
2024-03-01 18:07:05 +0530

    add user and video model

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 import {DB_NAME } from "../constants.js";
+import { app } from "../app.js";
 
 const connectDB = async () => {
     try {
-        console.log(process.env.MONGODB_URI)
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
         // console.log(connectionInstance);
@@ -14,4 +14,3 @@ const connectDB = async () => {
 }
 
 export default connectDB;
-
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 // require("dotenv").config();  // makes code inconsistent
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: '.env'
 });
 
-connectDB();
+connectDB()
+.then(() => {
+
+    app.on("error", (err) => {
+        console.log("Server Crashed!!", err);
+        throw err;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on PORT: ${process.env.PORT || 8000}`);
+    });
+})
+.catch((err) => {
+    console.log("MONGODB connection FAILED!!"), err;
+})
 
